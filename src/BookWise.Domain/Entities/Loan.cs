fe82add201605a0d112e6a5e3942837121946657
Christhian Lopes <christhian.lopes@catalde.com>
2024-12-22 12:38:22 -0300

    Add loan entity

---
+++ src/BookWise.Domain/Entities/Loan.cs
@@ -0,0 +1,22 @@
+﻿namespace BookWise.Domain.Entities;
+
+public class Loan : Entity
+{
+    public Loan(
+        Reader reader,
+        Book book)
+    {
+        Reader = reader;
+        Book = book;
+        LoanDate = DateTime.Now.Date;
+    }
+
+    public DateTime LoanDate { get; private set; }
+    public DateTime? ReturnDate { get; private set; }
+
+    public int ReaderId { get; private set; }
+    public Reader Reader { get; private set; } = null!;
+
+    public int BookId { get; private set; }
+    public Book Book { get; private set; } = null!;
+}
