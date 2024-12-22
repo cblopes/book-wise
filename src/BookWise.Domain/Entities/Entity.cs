ed7f217b133909f42afb0a0b15ffec994835d8cf
Christhian Lopes <christhian.lopes@catalde.com>
2024-12-22 12:37:08 -0300

    Add reader entity

---
+++ src/BookWise.Domain/Entities/Entity.cs
@@ -0,0 +1,6 @@
+﻿namespace BookWise.Domain.Entities;
+
+public abstract class Entity
+{
+    public int Id { get; private set; }
+}
---
+++ src/BookWise.Domain/Entities/Reader.cs
@@ -0,0 +1,22 @@
+﻿namespace BookWise.Domain.Entities;
+
+public class Reader : Entity
+{
+    public Reader(
+        string firstName,
+        string lastName,
+        DateTime birthDate)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        BirthDate = birthDate.Date;
+        LoanAmount = 0;
+        IsSuspended = false;
+    }
+
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public DateTime BirthDate { get; private set; }
+    public int LoanAmount { get; private set; }
+    public bool IsSuspended { get; private set; }
+}
